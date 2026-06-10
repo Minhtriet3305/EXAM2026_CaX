7dc24efba6c0015a4e8008fe6a1f9974b94ffa7f
Minhtriet3305 <leminhtriet343@gmail.com>
2026-06-10 08:13:30 +0700

    Đã tạo file js chứa mảng và thêm form thêm sản phẩm

---
+++ html/data/data.js
@@ -0,0 +1,41 @@
+
+
+const products = [
+    {
+        id: "1",
+        name: "Điện thoại iPhone 15",
+        category: "Điện thoại",
+        price: 1250000,
+        status: "Còn hàng",
+    },
+    {
+        id: "2",
+        name: "Laptop Dell XPS 13",
+        category: "Laptop",
+        price: 1500000,
+        status: "Còn hàng",
+    },
+    {
+        id: "3",
+        name: "Máy tính bảng Samsung Galaxy Tab S9",
+        category: "Máy tính bảng",
+        price: 1850000,
+        status: "Còn hàng",
+    },
+    {
+        id: "4",
+        name: "Máy tính xách tay Macbook Air M3",
+        category: "Laptop",
+        price: 3190000,
+        status: "Còn hàng",
+    },
+    {
+        id: "5",
+        name: "Tai nghe Sony WH-1000XM5",
+        category: "Tai nghe",
+        price: 799000,
+        status: "Còn hàng",
+    }
+];
+
+
